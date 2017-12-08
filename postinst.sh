e6f9d72581e75407e7b41448ca29379981bd600e
marekpl <marekpl@nask.pl>
2017-12-08 21:44:23 +0100

    deleted:    d89.cfg 	new file:   grub 	modified:   postinst.sh 	modified:   preseed.cfg 	modified:   v 	new file:   vr

--- postinst.sh
+++ postinst.sh
@@ -3,7 +3,11 @@
 # This script is run by debian installer using preseed/late_command
 # directive, see preseed.cfg
 
-# Setup console, remove timeout on boot.
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS0"/g; s/TIMEOUT=5/TIMEOUT=0/g' /etc/default/grub
+mv /etc/default/grub /etc/default/grub.orig
+mv /tmp/grub /etc/default/grub
 update-grub
+
+echo >> /etc/motd
+ip address show | grep -w inet >> /etc/motd
+echo >> /etc/motd
 
