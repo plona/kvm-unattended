139fb7fa8a231061fa912b1d6697ea7c5101716b
marekpl <marekpl@nask.pl>
2017-12-11 18:27:01 +0100

    Added ansible playbook Added ansible playbook 	modified:   .gitignore 	new file:   debian-new/ansible.cfg 	new file:   debian-new/files/home/.bashrc 	new file:   debian-new/files/home/.config/mc/Tree 	new file:   debian-new/files/home/.config/mc/hotlist 	new file:   debian-new/files/home/.config/mc/ini 	new file:   debian-new/files/home/.config/mc/panels.ini 	new file:   debian-new/files/home/.profile 	new file:   debian-new/files/home/.psqlrc 	new file:   debian-new/install.yml 	new file:   debian-new/inventory/hosts.ini 	new file:   debian-new/local_scripts/post-install.sh 	new file:   debian-new/local_scripts/pre-install.sh 	new file:   debian-new/ping.yml 	new file:   debian-new/pre-install.yml 	new file:   debian-new/reboot.yml 	new file:   debian-new/sysvinit.yml 	new file:   debian-new/templates/etc/apt/preferences.d/systemd 	new file:   debian-new/templates/etc/apt/sources.list 	new file:   debian-new/templates/etc/bash_completion.d/tmux 	new file:   debian-new/templates/etc/screenrc 	new file:   debian-new/templates/etc/tmux.conf 	new file:   debian-new/templates/usr/local/bin/screen.backtick

---
+++ debian-new/local_scripts/post-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: post-install.sh
+# 
+#         USAGE: ./post-install.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Marek Płonka (marekpl), marek.plonka@nask.pl
+#  ORGANIZATION: NASK
+#       CREATED: 11/17/2017 09:07:20 AM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+set -o errexit
+
+files="files"
+
+rm -rf $files/home/.vim || true
+rm -f $files/*.tgz
+
+exit 0
